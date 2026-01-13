a07c8492467255b5b63f2b1b8454999c4c42da00
Roshan Patil <patilroshan9687@gmail.com>
2026-01-13 13:55:17 +0530

    import and export Home component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Input from "./components/Input/Input";
 import SignUp from "./components/SignUp/SignUp";
 import Login from "./components/LogIn/Login";
+import Home from "./components/Home/Home";
 
-export {Input, SignUp, Login}
+export {Input, SignUp, Login,Home}
